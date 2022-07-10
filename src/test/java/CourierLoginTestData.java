85bf981673be806ca58e8327cc153f41310170c5
comaBblack <marinaguzhanova@yandex.com>
2022-07-10 23:12:33 +0300

    Add all API test

---
+++ src/test/java/CourierLoginTestData.java
@@ -0,0 +1,11 @@
+public class CourierLoginTestData {
+    public final String login;
+    public final String password;
+
+
+    public CourierLoginTestData(String login, String password){
+        this.login = login;
+        this.password = password;
+
+    }
+}
